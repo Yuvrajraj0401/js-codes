139377ffefd4f9f0b432ec79a2faed42c9e5b960
Yuvraj Raj <102218175+Yuvrajraj0401@users.noreply.github.com>
2025-01-17 11:58:09 +0000

    DataTypes

---
+++ 01_basic/02basics.js
@@ -0,0 +1,15 @@
+// Data type
+"use strict"; //this uses the newer version of Java Script
+const name="yuvraj"
+let age=20
+let Place= "Ranchi"
+// number = 2 to power 53
+// bigint
+// String=" "
+// boolean= true/false
+// null=standalone value
+// undefined => undefined
+// symbol =>unique
+console.table([age,Place,name])
+console.log(typeof null);
+
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,10 @@
-console.log("Yuvraj")
+let accountId = "123"
+const accountName= "Yuvraj"
+var accountRes="Akansha"
+accountThings="ring"
+
+accountId="345"
+// tetsaccountName="radhe"
+accountRes="ashutosh"
+accountThings="Chain"
+console.log([accountId,accountName,accountRes,accountThings])
