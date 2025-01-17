6c9c39fc70df1f81e3fc30a0c3440840ac074ae0
Yuvraj Raj <102218175+Yuvrajraj0401@users.noreply.github.com>
2025-01-17 12:07:04 +0000

    typeconversion

---
+++ 01_basic/03TypeConversion.js
@@ -0,0 +1,13 @@
+"use strict"
+// let Number = 33
+// console.log(typeof Number)
+// let NumberinString=String(Number)
+// console.log(typeof NumberinString)
+let Booleans=0
+console.log(typeof Boolean)
+let Booleantonumber = Boolean(Booleans)
+console.log(typeof Booleantonumber)
+console.log(Booleantonumber);
+
+
+
